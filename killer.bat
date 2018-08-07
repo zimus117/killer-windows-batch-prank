917fe9a6bfa2a02f43c5c925b9ba9879ad4547d6
zimus117 <37185692+zimus117@users.noreply.github.com>
2018-08-07 15:46:07 +0500

    Update killer.bat

--- killer.bat
+++ killer.bat
@@ -8,3 +8,32 @@ taskkill /f /im steam.exe
 cls
 taskkill /f /im virtualbox.exe
 cls
+
+echo                      :PB@Bk:
+echo                  ,jB@@B@B@B@BBL.
+echo               7G@B@B@BMMMMMB@B@B@Nr
+echo           :kB@B@@@MMOMOMOMOMMMM@B@B@B1,
+echo       :5@B@B@B@BBMMOMOMOMOMOMOMM@@@B@B@BBu.
+echo    70@@@B@B@B@BXBBOMOMOMOMOMOMMBMPB@B@B@B@B@Nr
+echo  G@@@BJ iB@B@@  OBMOMOMOMOMOMOM@2  B@B@B. EB@B@S
+echo  @@BM@GJBU.  iSuB@OMOMOMOMOMOMM@OU1:  .kBLM@M@B@
+echo  B@MMB@B       7@BBMMOMOMOMOMOBB@:       B@BMM@B
+echo  @@@B@B         7@@@MMOMOMOMM@B@:         @@B@B@
+echo  @@OLB.          BNB@MMOMOMM@BEB          rBjM@B
+echo  @@  @     0     M  OBOMOMM@q  M     0    .@  @@
+echo  @@OvB           B:u@MMOMOMMBJiB          .BvM@B
+echo  @B@B@J         0@B@MMOMOMOMB@B@u         q@@@B@
+echo  B@MBB@v       G@@BMMMMMMMMMMMBB@5       F@BMM@B
+echo  @BBM@BPNi   LMEB@OMMMM@B@MMOMM@BZM7   rEqB@MBB@
+echo  B@@@BM  B@B@B  qBMOMB@B@B@BMOMBL  B@B@B  @B@B@M
+echo   J@@@@PB@B@B@B7G@OMBB.   ,@MMM@qLB@B@@@BqB@BBv
+echo      iGB@,i0@M@B@MMO@E  :  M@OMM@@@B@Pii@@N:
+echo         .   B@M@B@MMM@B@B@B@MMM@@@M@B
+echo             @B@B.i@MBB@B@B@@BM@::B@B@
+echo             B@@@ .B@B.:@B@ :B@B  @B@O
+echo               :0 r@B@  B@@ .@B@: P:
+echo                   vMB :@B@ :BO7
+echo
+echo                   you hacked!
+
+pause
